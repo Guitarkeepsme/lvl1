2845b067c135e5956f6bc207c80fcf40b34f1677
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-09-10 14:33:53 +0300

    task4 add

--- tasks/task4/task4.go
+++ tasks/task4/task4.go
@@ -5,6 +5,44 @@
 */
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	// Сначала создаём канал, который будет передавать данные между воркерами
+	dataChan := make(chan int)
+
+	// Указываем количество воркеров, например 3
+	workerCount := 3
+
+	// Создаём WaitGroup для ожидания завершения всех воркеров
+	var wg sync.WaitGroup
+	wg.Add(workerCount)
+
+	// Запускаем воркеров и проходим по ним циклом
+	for i := 0; i < workerCount; i++ {
+		go worker(i, dataChan, &wg)
+	}
+
+	// Главный поток записывает данные в канал
+	go func() {
+		for i := 0; ; i++ {
+			dataChan <- i
+		}
+	}()
+
+	// Наконец, ждём, когда все воркеры завершат работу
+	wg.Wait()
+}
+
+// Функция, создающая воркеров
+func worker(id int, dataChan <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// Читаем данные из канала и выводим их в stdout
+	for data := range dataChan {
+		fmt.Printf("Worker %d: %d\n", id, data)
+	}
 }
