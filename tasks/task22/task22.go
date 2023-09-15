c3e9157967332fb4a6f46aeca0f58960766f1ee7
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-09-15 13:16:43 +0300

    task 22 done

--- tasks/task22/task22.go
+++ tasks/task22/task22.go
@@ -1,4 +1,5 @@
 // Разработать программу, которая перемножает, делит, складывает, вычитает две числовых переменных a,b, значение которых > 2^20.
+
 package main
 
 import (
@@ -9,30 +10,38 @@ import (
 )
 
 func main() {
-	defA := "283904503245"
-	defB := "9896954876927"
+	// В качестве дефолтных значений берём величины, явно превышающие 2^20
+	defA := "28390450324533"
+	defB := "9896954876927234"
 
+	// Инициализируем флаги для выбора кастомных значений
 	a := flag.String("a", defA, "Введите число а")
 	b := flag.String("b", defB, "Введите число b")
+	// И флаг для операции
+	oper := flag.String("oper", "sum", "Выберите операцию: \nsum - Сложениe\tsub - Вычитание\nmul - Умножение\tdiv - Деление\n")
 	flag.Parse()
-
+	// Теперь переводим величины в тип данных decimal для дальнейших вычислений
 	parsedA, _ := decimal.NewFromString(*a)
 	parsedB, _ := decimal.NewFromString(*b)
 
+	// Проводим операции с ними
 	sum := parsedA.Add(parsedB)
 	sub := parsedA.Sub(parsedB)
 	mul := parsedA.Mul(parsedB)
 	div := parsedA.Div(parsedB)
 
-	oper := flag.String("operation", sum.String(), "Выберите операцию: \n1 - Сложениe\t2 - Вычитание\n3 - Умножение\t4 - Деление\n")
-	sumF := flag.String("sum", sum.String(), "Сумма двух чисел")
-	subF := flag.String("sub", sub.String(), "Разность двух чисел")
-	mulF := flag.String("mul", mul.String(), "Умножение двух чисел")
-	divF := flag.String("div", div.String(), "Деление двух чисел")
-	flag.Parse()
+	// Создаём условия для выбора операции
+	switch *oper {
+	case "sum":
+		fmt.Printf("%s\n", sum)
+	case "sub":
+		fmt.Printf("%s\n", sub)
+	case "mul":
+		fmt.Printf("%s\n", mul)
+	case "div":
+		fmt.Printf("%s\n", div)
+	default:
+		fmt.Printf("%s\n", sum)
+	}
 
-	fmt.Printf("%s\n", *sumF)
-	fmt.Printf("%s\n", *subF)
-	fmt.Printf("%s\n", *mulF)
-	fmt.Printf("%s\n", *divF)
 }
