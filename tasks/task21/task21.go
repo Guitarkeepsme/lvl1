b8aceafd486997276a9755cf847a3d9e13ea81d4
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-09-17 13:50:00 +0300

    commented out task 21

--- tasks/task21/task21.go
+++ tasks/task21/task21.go
@@ -2,42 +2,40 @@
 
 package main
 
-import "fmt"
+// import "fmt"
 
 func main() {
-	rusUser := &RusUser{}
-	application := &Application{}
-	rusUser.rusUserTypesReq(application)
+	// rusUser := &RusUser{}
+	// application := &Application{}
+	// rusUser.rusUserTypesReq(application)
 }
 
+// type RusUser struct {
+// }
 
+// func (ru *RusUser) rusUserTypesReq(app RusText) {
+// 	fmt.Println("User types a request in Russian.")
+// 	app.RusText()
+// }
 
-type RusUser struct {
-}
+// type RusText interface {
+// 	RusText()
+// }
 
-func (ru *RusUser) rusUserTypesReq(app RusText) {
-	fmt.Println("User types a request in Russian.")
-	app.RusText()
-}
+// type Application struct {
+// }
 
-type RusText interface {
-	RusText()
-}
+// func (ts *Application) reqestSend() {
+// 	fmt.Println("A request is sent to the server.")
+// }
 
-type Application struct {
-}
+// type
 
-func (ts *Application) reqestSend() {
-	fmt.Println("A request is sent to the server.")
-}
+// type autoTranslator struct {
+// 	Translator *Application
+// }
 
-type 
-
-type autoTranslator struct {
-	Translator *Application
-}
-
-func (at *autoTranslator) requestInEnglish() {
-	fmt.Println("A request has been translated and done.")
-	at.Translator.reqestSend()
-}
+// func (at *autoTranslator) requestInEnglish() {
+// 	fmt.Println("A request has been translated and done.")
+// 	at.Translator.reqestSend()
+// }
