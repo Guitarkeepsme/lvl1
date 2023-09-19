1cf35637254273f8de22929a90b94a1c595ccc09
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-09-19 10:04:18 +0300

    task 21 rewritten

--- tasks/task21/task21.go
+++ tasks/task21/task21.go
@@ -1,69 +1,47 @@
-// Реализовать паттерн «адаптер» на любом примере.
-
 package main
 
 import "fmt"
 
-// Пусть есть интерфейс MediaPlayer с методом Play:
-
-type MediaPlayer interface {
-	Play(audioType string, fileName string) error
+// Имеющийся интерфейс
+type EnglishSpeaker interface {
+	SpeakEnglish() string
 }
 
-// Нам хочется создать новую версию плеера под названием
-// AdvancedMediaPlayer, которая будет воспроизводить
-// не только аудио, но и видео:
-type AdvancedMediaPlayer interface {
-	PlayAudio(filename string) error
-	PlayVideo(filename string) error
+// Структура, реализующая имеющийся интерфейс
+type EnglishPerson struct{}
+
+func (e *EnglishPerson) SpeakEnglish() string {
+	return "Hello!"
 }
 
-// Допустим, есть устройство VLCPlayer, которое поддерживает
-// новый плеер, но к нему нельзя подключить старый:
-type VLCPlayer struct{}
-
-func (v *VLCPlayer) PlayVideo(fileName string) error {
-	fmt.Println("Воспроизведение видеофайла под названием: ", fileName)
-	return nil
+// Целевой интерфейс
+type RussianSpeaker interface {
+	SpeakRussian() string
 }
 
-func (v *VLCPlayer) PlayAudio(fileName string) error {
-	fmt.Println("Воспроизведение аудиофайла под названием: ", fileName)
-	return nil
+// Адаптер
+type Adapter struct {
+	englishSpeaker EnglishSpeaker
 }
 
-// Чтобы подключить старый плеер к этому устройству,
-// потребуется адаптер:
-type MediaAdapter struct {
-	advancedMusicPlayer AdvancedMediaPlayer
+func (a *Adapter) SpeakRussian() string {
+	// Вызываем метод SpeakEnglish у имеющегося интерфейса
+	englishText := a.englishSpeaker.SpeakEnglish()
+	// Производим необходимые преобразования для получения русского текста
+	russianText := convertEnglishToRussian(englishText)
+	return russianText
 }
 
-func (m *MediaAdapter) Play(audioType string, filename string) error {
-	if audioType == "vlc" {
-		return m.advancedMusicPlayer.PlayVideo(filename)
-	} else if audioType == "mp4" {
-		return m.advancedMusicPlayer.PlayAudio(filename)
-	}
-	return nil
+// Функция для преобразования английского текста в русский
+func convertEnglishToRussian(englishText string) string {
+	// Здесь может быть реализована логика преобразования текста
+	return "Привет!"
 }
 
-type AudioPlayer struct {
-	mediaAdapter MediaPlayer
-}
-
-func (a *AudioPlayer) Play(audioType string, fileName string) error {
-	// Inbuilt support for mp3 music files
-	if audioType == "mp3" {
-		fmt.Printf("Playing mp3 file. Name: %s\n", fileName)
-		return nil
-	} else if audioType == "vlc" || audioType == "mp4" {
-		a.mediaAdapter = &MediaAdapter{&VLCPlayer{}}
-		return a.mediaAdapter.Play(audioType, fileName)
-	}
-
-	return fmt.Errorf("invalid media. %s format not supported", audioType)
-}
-
+// Клиентский код
 func main() {
-
+	englishPerson := &EnglishPerson{}
+	adapter := &Adapter{englishSpeaker: englishPerson}
+	russianText := adapter.SpeakRussian()
+	fmt.Println(russianText) // Выводит: Привет!
 }
